195377163b6dcf032ac0f9db350de791469e6e97
Wan Amier Haziq <amrhzq05@gmail.com>
2024-04-27 14:15:59 +0800

    Add : menu screen

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-# Example file showing a basic pygame "game loop"
-import pygame
+from sandbox import Game
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+g = Game()
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
+while g.running:
+    g.playing = True
+    g.game_loop()
