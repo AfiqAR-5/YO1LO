a4f36b205ed1e2ab56752c6265f3b558d245a675
Wan Amier Haziq <amrhzq05@gmail.com>
2024-05-03 00:34:39 +0800

    Fix some buttons

--- mainmenu.py
+++ mainmenu.py
@@ -6,7 +6,7 @@ pygame.init()
 pygame.display.set_caption('Menu')
 
 class Button:
-    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color, scale):
         pygame.init()
         self.screen = config.SCREEN
         self.font = pygame.font.Font('ARCADECLASSIC.TTF', 32)
@@ -18,6 +18,7 @@ class Button:
         self.x_pos = pos[0]
         self.y_pos = pos[1]
         self.font = font
+        self.scale = scale
         self.base_color, self.hovering_color = base_color, hovering_color
         self.text_input = text_input
         self.text = self.font.render(self.text_input, True, self.base_color)
@@ -52,7 +53,7 @@ class Button:
         return False
         
 
-    def character_select(self):
+    def character_select(self,scale):
         char_sel = True
         choosecharacterbg = pygame.image.load('choosecharbg.jpg')
         title = self.font.render("Choose your story", True, "White")
@@ -60,20 +61,33 @@ class Button:
         self.screen.blit(choosecharacterbg, (0,0))
         self.screen.blit(title, title_rect)
 
-        mc_pic = pygame.image.load('mc.png')
-        mc_rect = mc_pic.get_rect(x=150, y=250)
+        #mc_pic = pygame.image.load('mc.png')
+      
 
-        lover_pic = pygame.image.load('lover.png')
-        lover_rect = lover_pic.get_rect(x=300, y=250)
+        #lover_pic = 
+        #lover_rect = lover_pic.get_rect(x=300, y=250)
 
-        villain_pic = pygame.image.load('villain.png')
+        #villain_pic = pygame.image.load('villain.png')
+        #villain_rect = villain_pic.get_rect(x=450, y=250)
+
+        self.scale = scale
+
+        lover = Button(image=pygame.image.load('lover.png'), pos=(0, 0), text_input="LOVER", font=Button.get_font(45), base_color="#d7fcd4", hovering_color="White", scale=0.01)
+        lover_scale = pygame.transform.scale(lover.image, (lover.image.get_size()))
+        lover_rect = lover_scale.get_rect(x=0, y=0)
+        
+        mc = Button(image=pygame.image.load('mc.png'), pos=(400, 320), text_input="MC", font=Button.get_font(45), base_color="#d7fcd4", hovering_color="White", scale=0.01)
+        mc_button = pygame.image.load('mc.png')
+        mc_pic = pygame.surface.Surface(mc_button.get_size())
+        mc_rect = mc_pic.get_rect(x=400, y=250)
+        
+        villain = Button(image=pygame.image.load('villain.png'), pos=(420, 320), text_input="VILLAIN", font=Button.get_font(45), base_color="#d7fcd4", hovering_color="White", scale=0.01)
+        villain_button = pygame.image.load('villain.png')
+        villain_pic = pygame.surface.Surface(villain_button.get_size())
         villain_rect = villain_pic.get_rect(x=450, y=250)
 
-        lover_button = Button(image=pygame.image.load("Play Rect.png"), pos=(120, 320), text_input="LOVER", font=Button.get_font(45), base_color="#d7fcd4", hovering_color="White")
-        mc_button = Button(image=pygame.image.load("Play Rect.png"), pos=(270, 320), text_input="MC", font=Button.get_font(45), base_color="#d7fcd4", hovering_color="White")
-        villain_button = Button(image=pygame.image.load("Play Rect.png"), pos=(420, 320), text_input="VILLAIN", font=Button.get_font(45), base_color="#d7fcd4", hovering_color="White")
 
-        back_button = Button(image=pygame.image.load("Play Rect.png"), pos=(200, 70), text_input="BACK", font=Button.get_font(45), base_color="#d7fcd4", hovering_color="White")
+        back_button = Button(image=pygame.image.load("Play Rect.png"), pos=(400, 400), text_input="BACK", font=Button.get_font(45), base_color="#d7fcd4", hovering_color="White", scale=0.01)
         while char_sel:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -85,29 +99,26 @@ class Button:
     
             if back_button.is_pressed(mouse_pos,mouse_pressed):
                 Button.main_menu()
-            elif mc_button.is_pressed(mouse_pos,mouse_pressed):
+            elif mc.is_pressed(mouse_pos,mouse_pressed):
                 char_sel = False
                 self.char_selection = 1
                 print("Proceed to story Chap. 1...")
-            elif lover_button.is_pressed(mouse_pos,mouse_pressed):
+            elif lover.is_pressed(mouse_pos,mouse_pressed):
                 char_sel = False
                 self.char_selection = 2
                 print("Complete Story : MC...")
-            elif mc_button.is_pressed(mouse_pos,mouse_pressed):
+            elif villain.is_pressed(mouse_pos,mouse_pressed):
                 char_sel = False
                 self.char_selection = 3
-                print("Proceed to Story : MC...")
+                print("Complete Story : MC...")
             title = self.font.render("Choose your story", True, "White")
             title_rect = title.get_rect(x=100, y=100)
             choosecharacterbg = pygame.image.load('choosecharbg.jpg')
             if back_button.image is not None:
                 self.screen.blit(back_button.image, back_button.rect)
-            # self.screen.blit(mc_button, mc_button.rect)
-            # self.screen.blit(lover_button, lover_button.rect)
-            # self.screen.blit(villain_button, villain_button.rect)
-            # self.screen.blit(mc_pic, mc_rect)
-            # self.screen.blit(lover_pic, lover_rect)
-            # self.screen.blit(villain_pic, villain_rect)
+            self.screen.blit(mc.image, mc_rect)
+            self.screen.blit(lover.image, lover_rect)
+            self.screen.blit(villain.image, villain_rect)
             self.clock.tick(60)
         pygame.display.update()
 
@@ -117,11 +128,11 @@ class Button:
             play_mouse_pos = pygame.mouse.get_pos()
 
             config.SCREEN.fill('black')
-            button = Button(image=None, pos=(400, 260), text_input="BACK", font=Button.get_font(75), base_color="White", hovering_color="Green")
-            button.character_select()
+            button = Button(image=None, pos=(400, 260), text_input="BACK", font=Button.get_font(75), base_color="White", hovering_color="Green", scale = 1)
+            button.character_select(scale=1)
 
 
-            PLAY_BACK = Button(image=None,pos=(400,260),text_input="BACK", font=Button.get_font(75), base_color="White", hovering_color="Green")
+            PLAY_BACK = Button(image=None,pos=(400,260),text_input="BACK", font=Button.get_font(75), base_color="White", hovering_color="Green", scale = 1)
 
             PLAY_BACK.changeColor(play_mouse_pos) 
             PLAY_BACK.update(config.SCREEN)
@@ -148,8 +159,8 @@ class Button:
 
             MENU_TEXT = Button.get_font(100).render("YO1LO", True,"#b68f40")
             MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))   
-            PLAY_BUTTON = Button(image=pygame.image.load("Play Rect.png"), pos=(400, 250), text_input="PLAY", font=Button.get_font(75), base_color="#d7fcd4", hovering_color="White")
-            QUIT_BUTTON=Button(image=pygame.image.load("Quit Rect.png"), pos=(400,400), text_input="QUIT", font=Button.get_font(75),base_color="#d7fcd4", hovering_color="White")
+            PLAY_BUTTON = Button(image=pygame.image.load("Play Rect.png"), pos=(400, 250), text_input="PLAY", font=Button.get_font(75), base_color="#d7fcd4", hovering_color="White", scale = 1)
+            QUIT_BUTTON=Button(image=pygame.image.load("Quit Rect.png"), pos=(400,400), text_input="QUIT", font=Button.get_font(75),base_color="#d7fcd4", hovering_color="White", scale = 1)
             
             config.SCREEN.blit(MENU_TEXT, MENU_RECT) 
 
