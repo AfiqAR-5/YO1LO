c78ca00345a2ced29c10555f31c9facbbf60dcf2
AfiqAR-5 <afiqarrasyid0510@gmail.com>
2024-05-06 23:56:40 +0800

    finalized main menu

--- mainmenu.py
+++ mainmenu.py
@@ -1,5 +1,5 @@
 import pygame, sys
-from button import Button
+from menubutton import Button
 
 pygame.init()
 
@@ -72,7 +72,7 @@ def credits():
         CREDITS_WILLIE = ("Lecturer and Teacher              Mr Willie")
         CREDITS_AFIQ = ("Storyboarding                                Afiq")
         CREDITS_CANDU = ("Game Mechanics                       Wan Amier")
-        CREDITS_AMIR = ("Data Sorter                         Amir Asyraf")
+        CREDITS_AMIR = ("Data Sorter                        Amir Asyraf")
 
         CREDITS_TEXT1 = get_font(50).render(CREDITS_WILLIE, True, "White")
         CREDITS_TEXT2 = get_font(50).render(CREDITS_AFIQ, True, "White")
